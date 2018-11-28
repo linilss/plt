f7f83220d44f17ef1b908f81b53969e2efd65cbe
linilss <linuni@student.chalmers.se>
2018-11-28 13:59:55 +0100

    well blocks within blocks maybe working

--- test/const.cc
+++ test/const.cc
@@ -1,8 +1,12 @@
-    int scope_violation (int x) {
-      int x = 1;
+int main () {
+  int i = 6 ;
+  {
+      int i = 2 ;
       {
-        int y = 2;
-        int y;
+        int i = 3;
+        {
+        }
       }
-      return x;
+      i++ ;
     }
+}
